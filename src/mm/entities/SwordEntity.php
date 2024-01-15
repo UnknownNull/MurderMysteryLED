7048560fbdb6541c61d1edabd82d223aac05becd
testdev <83214438+DanteDEVS@users.noreply.github.com>
2024-01-15 19:14:41 +0000

    eh

--- src/mm/entities/SwordEntity.php
+++ src/mm/entities/SwordEntity.php
@@ -4,12 +4,13 @@ namespace mm\entities;
 
 use pocketmine\entity\Entity;
 use pocketmine\entity\EntitySizeInfo;
-use pocketmine\item\VanillaItems;
+use pocketmine\item\{VanillaItems, ItemTypeIds};
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStack;
 use pocketmine\player\Player;
 use pocketmine\network\mcpe\protocol\MobEquipmentPacket;
 use pocketmine\network\mcpe\protocol\types\inventory\ItemStackWrapper;
+use pocketmine\world\format\io\GlobalItemDataHandlers;
 use pocketmine\item\Item;
 
 class SwordEntity extends Entity{
@@ -19,7 +20,7 @@ class SwordEntity extends Entity{
     }
 
     protected function getInitialGravity(): float {
-        return 0.04;
+        return 0.01;
     }
 
     public static function getNetworkTypeId(): string
@@ -36,12 +37,15 @@ class SwordEntity extends Entity{
 
     public $width = 2.0;
     public $height = 2.0;
-    
+
+    // This feature may aswell stay on hold because PMMP has an issue
+    // [21:12:21.288] [Server thread/CRITICAL]: Error: "Call to undefined method pocketmine\network\mcpe\protocol\MobEquipmentPacket::getTypeId()" 
+    // (EXCEPTION) in "pmsrc/vendor/pocketmine/bedrock-protocol/src/serializer/PacketSerializer" at line 453
     protected function sendSpawnPacket(Player $player) : void{
         parent::sendSpawnPacket($player);
         $pk = new MobEquipmentPacket();
         $pk->actorRuntimeId = $this->getId();
-        $pk->item = ItemStackWrapper::legacy(new ItemStack(VanillaItems::IRON_SWORD(), 0, 1, 0,null, null, null));
+        $pk->item = ItemStackWrapper::legacy(new ItemStack(20138, 0, 1, 0, null, [], []));
         $pk->inventorySlot = 0;
         $pk->hotbarSlot = 0;
         $player->sendData($this->getViewers(), [$pk]);
