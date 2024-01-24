9de5292e67b51b39ead3f95a52907211e2dbfc6f
ItsToxic <95104544+ItsToxicGG@users.noreply.github.com>
2024-01-24 21:03:22 +0000

    #6 integration after 2 years

--- src/mm/game/GameTask.php
+++ src/mm/game/GameTask.php
@@ -14,7 +14,7 @@ use pocketmine\block\tile\Sign;
 use pocketmine\block\utils\SignText;
 use pocketmine\player\GameMode;
 use pocketmine\block\VanillaBlocks;
-
+use pocketmine\player\Player;
 use mm\utils\Vector;
 
 class GameTask extends Task{
@@ -258,7 +258,7 @@ class GameTask extends Task{
             $sign->setText(new SignText([$signText[0], $signText[1], $signText[2], $signText[3]]));
     }
 
-    public function addSound($player, string $sound = '', float $pitch = 1){
+    public function addSound(Player $player, string $sound = '', float $pitch = 1){
         $pk = new PlaySoundPacket();
         $pk->x = $player->getPosition()->getX();
         $pk->y = $player->getPosition()->getY();
